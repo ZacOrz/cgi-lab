34da64e4355bf9adc7dc782f7cf816d70f8a505e
hhong1 <44427485+ZacOrz@users.noreply.github.com>
2021-09-24 08:54:22 -0600

    Add files via upload

---
+++ login.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+import json
+from templates import login_page, secret_page, after_login_incorrect
+import sys
+import cgi
+import secret
+
+form = cgi.FieldStorage()
+username = form.getvalue("username")
+password = form.getvalue("password")
+
+if (username == secret.username and password == secret.password):
+    print(f"Set-Cookie:username = {username}")
+    print(f"Set-Cookie:password = {password}")
+    print("Content-Type: text/html\r\n")
+else:
+    print(after_login_incorrect())
